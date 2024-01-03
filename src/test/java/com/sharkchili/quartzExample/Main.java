de514c9b9bdacd6028d1fd5fed385ee59a5c3a74
shark-chili <1203783447@qq.com>
2024-01-03 17:42:30 +0800

    refactor  :调试代码

--- src/test/java/com/sharkchili/quartzExample/Main.java
+++ src/test/java/com/sharkchili/quartzExample/Main.java
@@ -1,27 +1,45 @@
 package com.sharkchili.quartzExample;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) throws Exception {
         // 获取任务调度的实例
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
-        // 定义任务调度实例, 并与TestJob绑定
-        JobDetail job = JobBuilder.newJob(MyJob.class)
-                .withIdentity("myJob", "myJobGroup")
-                .build();
+        for (int i = 0; i < 10; i++) {
+            int taskId = RandomUtil.randomInt(100);
 
-        // 定义触发器, 会马上执行一次, 接着1秒执行一次
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("testTrigger", "testTriggerGroup")
-                .startNow()
-                .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1))
-                .build();
+            // 定义任务调度实例, 并与TestJob绑定
+            JobDetail jobDetail = JobBuilder.newJob(MyJob.class)
+                    .withIdentity("myJob-" + taskId, "myJobGroup")
+                    .build();
+            //获取封装任务重要属性的类
+            JobDataMap jobDataMap = jobDetail.getJobDataMap();
+            String name = jobDetail.getKey().getName();
+            String group = jobDetail.getKey().getGroup();
+            String jobClassName = jobDetail.getJobClass().getName();
+            log.info("jobDataMap:{},name:{},group:{},jobClassName:{}", JSONUtil.toJsonStr(jobDataMap), name, group, jobClassName);
 
-        // 使用触发器调度任务的执行
-        scheduler.scheduleJob(job, trigger);
+            jobDataMap.put("id", taskId);
+
+            // 定义触发器, 会马上执行一次, 接着60秒执行一次
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    //在name重复的情况下会报错
+                    .withIdentity("testTrigger-" + taskId, "testTriggerGroup")
+                    .startNow()
+                    .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(10))
+                    .build();
+
+            // 使用触发器调度任务的执行
+            scheduler.scheduleJob(jobDetail, trigger);
+        }
+
 
         // 开启任务
         scheduler.start();
--- src/test/java/com/sharkchili/quartzExample/MyJob.java
+++ src/test/java/com/sharkchili/quartzExample/MyJob.java
@@ -1,5 +1,6 @@
 package com.sharkchili.quartzExample;
 
+import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -11,8 +12,7 @@ import java.time.format.DateTimeFormatter;
 public class MyJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
-        String dateTime = LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        log.info("任务执行时间:{}", dateTime);
+        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        log.info("任务执行时间:{} ，jobExecutionContext:{}", dateTime, JSONUtil.toJsonStr(jobExecutionContext.getJobDetail().getJobDataMap()));
     }
 }
